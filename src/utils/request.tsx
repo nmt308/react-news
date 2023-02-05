b163b1420967d416f1238d6d344931fe5519a3bc
nmt308 <minhthanh0946@gmail.com>
2023-02-05 20:30:09 +0700

    Change https request

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const request = axios.create({
-    baseURL: 'http://eventregistry.org/api/v1',
+    baseURL: 'https://eventregistry.org/api/v1',
 });
 export default request;
